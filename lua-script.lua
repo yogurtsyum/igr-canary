7f3decde9d68291a18181e5540738c2084d8d896
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2020-07-21 11:34:15 -0500

    Add a prefix (!) and remove some debugging stuff

--- lua-script.lua
+++ lua-script.lua
@@ -18,71 +18,42 @@ function slice(tbl, first, last, step)
 end
 
 game.Players.PlayerAdded:connect(function(player)
-	print('binding chat')
 	player.Chatted:connect(function(msg)
 		local args = msg:split(" ")
-		if args[1] == "setrank" then
-			print('setrank running')
+		if args[1] == "!setrank" then
 			local user = args[2]
 			local rank = slice(args, 3)
 			rank = table.concat(rank, ' ')
 			if(user and rank) then
-				print('passed argument check')
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					print('passed rank check')
 					local res = HttpService:GetAsync(server..'setrank?user='..user..'&rank='..rank..'&key='..key..'&author='..player.Name)
-					print(res);
-					print('all done')
 				end
 			end
-		elseif args[1] == "promote" then
-			print('promote running')
+		elseif args[1] == "!promote" then
 			local user = args[2]
 			if(user) then
-				print('passed argument check')
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					print('passed rank check')
-					print(server..'setrank?user='..user..'&key'..key)
 					local res = HttpService:GetAsync(server..'promote?user='..user..'&key='..key..'&author='..player.Namey)
-					print(res);
-					print('all done')
 				end
 			end
-		elseif args[1] == "demote" then
-			print('demote running')
+		elseif args[1] == "!demote" then
 			local user = args[2]
 			if(user) then
-				print('passed argument check')
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					print('passed rank check')
-					print(server..'setrank?user='..user..'&key'..key)
 					local res = HttpService:GetAsync(server..'demote?user='..user..'&key='..key..'&author='..player.Name)
-					print(res);
-					print('all done')
 				end
 			end
-		elseif args[1] == "fire" then
-			print('fire running')
+		elseif args[1] == "!fire" then
 			local user = args[2]
 			if(user) then
-				print('passed argument check')
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					print('passed rank check')
 					local res = HttpService:GetAsync(server..'fire?user='..user..'&key='..key..'&author='..player.Name)
-					print(res);
-					print('all done')
 				end
 			end
-		elseif args[1] == "shout" then
-			print('shout running')
+		elseif args[1] == "!shout" then
 			msg = table.concat(args, ' ')
-			print('passed argument check')
 			if(player:GetRankInGroup(groupId) >= requiredRank) then
-				print('passed rank check')
-				print(server..'shout?msg='..user)
 				local res = HttpService:GetAsync(server..'shout?msg='..msg..'&key='..key..'&author='..player.Name)
-				print(res);
-				print('all done')
 			end
 		end
 	end)
