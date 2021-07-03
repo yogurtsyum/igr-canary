508a6b32946416191d4eac2bb6a504d1bc9c57fc
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2021-07-03 10:32:23 -0500

    Update lua-script.lua

--- lua-script.lua
+++ lua-script.lua
@@ -47,35 +47,35 @@ game.Players.PlayerAdded:connect(function(player)
 			rank = table.concat(rank, ' ')
 			if(user and rank) then
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					local res = HttpService:GetAsync(server..'setrank?user='..user..'&rank='..rank..'&key='..key..'&author='..player.Name)
+					local res = HttpService:PostAsync(server..'shout', '{ user: '..user..', key: '..key..', rank: '..key..', author: '..player.Name..' }')
 				end
 			end
 		elseif args[1] == "!promote" then
 			local user = args[2]
 			if(user) then
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					local res = HttpService:GetAsync(server..'promote?user='..user..'&key='..key..'&author='..player.Name)
+					local res = HttpService:PostAsync(server..'promote', '{ user: '..user..', key: '..key..', author: '..player.Name..' }')
 				end
 			end
 		elseif args[1] == "!demote" then
 			local user = args[2]
 			if(user) then
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					local res = HttpService:GetAsync(server..'demote?user='..user..'&key='..key..'&author='..player.Name)
+					local res = HttpService:PostAsync(server..'demote', '{ user: '..user..', key: '..key..', author: '..player.Name..' }')
 				end
 			end
 		elseif args[1] == "!fire" then
 			local user = args[2]
 			if(user) then
 				if(player:GetRankInGroup(groupId) >= requiredRank) then
-					local res = HttpService:GetAsync(server..'fire?user='..user..'&key='..key..'&author='..player.Name)
+					local res = HttpService:PostAsync(server..'fire', '{ user: '..user..', key: '..key..', author: '..player.Name..' }')
 				end
 			end
 		elseif args[1] == "!shout" then
 			table.remove(args, 1)
 			local msg = table.concat(args, ' ')
 			if(player:GetRankInGroup(groupId) >= requiredRank) then
-				local res = HttpService:GetAsync(server..'shout?msg='..msg..'&key='..key..'&author='..player.Name)
+				local res = HttpService:PostAsync(server..'shout', '{ user: '..user..', key: '..key..', author: '..player.Name..', msg: '..msg..' }')
 			end
 		end
 	end)
